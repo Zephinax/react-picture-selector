74ddbfb2e05a642ec75dcbe7d108b513658e7438
Zephinax <zephinax@gmail.com>
2025-09-02 16:05:04 +0330

    update some preview triks

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -21,12 +21,14 @@ const Modal: React.FC<ModalProps> = ({
     isOpen &&
     ReactDOM.createPortal(
       <div
-        className={`fixed inset-0 z-[999] bg-[#0000006c]/30 backdrop-blur-[1px] flex items-end md:items-center justify-center ${
+        onClick={handleClose} // کلیک روی بک‌دراپ
+        className={`fixed inset-0 z-[999] bg-[#0000006c]/30 backdrop-blur-[4px] flex items-end md:items-center justify-center ${
           isOpen ? "translate-y-0" : "translate-y-[100%] delay-500"
         }`}
       >
         <div
-          className={`relative bg-white z-[9999] rounded-t-2xl md:rounded-lg border-secondary-200 mx-0  transition-all duration-300 delay-200 ${
+          onClick={(e) => e.stopPropagation()} // جلوی بسته شدن موقع کلیک روی محتوا
+          className={`relative z-[9999] rounded-t-2xl md:rounded-lg border-secondary-200 mx-0  transition-all duration-300 delay-200 ${
             isOpen ? "opacity-100" : "opacity-0"
           } md:max-w-[90%] ${
             size === "sm"
@@ -42,23 +44,9 @@ const Modal: React.FC<ModalProps> = ({
               : ""
           }  max-md:!w-full ${className || ""}`}
         >
-          <div className="px-5">
-            <div className="flex pt-5 pb-2 justify-between bg-white items-center">
-              {typeof title === "string" ? (
-                <h3 className="!mb-0">{title}</h3>
-              ) : (
-                title
-              )}
-              <button
-                onClick={handleClose}
-                className="ml-0 block cursor-pointer"
-              >
-                <ImXing className="w-6 h-6 text-secondary-600" />
-              </button>
-            </div>
-          </div>
-          <hr></hr>
-          <div className={`p-5 max-h-[90svh] ${childrenClass} ${overflowY}`}>
+          <div
+            className={`max-h-[80svh] rounded-2xl ${childrenClass} ${overflowY}`}
+          >
             {children}
           </div>
         </div>
