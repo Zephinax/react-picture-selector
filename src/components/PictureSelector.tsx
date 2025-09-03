9e08367ed06b7c89c41170cd967177c74f20c44e
Zephinax <zephinax@gmail.com>
2025-09-03 10:48:42 +0330

    some fixes to the picture selector component

--- src/components/PictureSelector.tsx
+++ src/components/PictureSelector.tsx
@@ -15,6 +15,8 @@ interface ColorPalette {
   error: string;
   progress: string;
   placeholder: string;
+  text: string;
+  textDisabled: string;
 }
 
 const PictureSelector = ({
@@ -32,9 +34,12 @@ const PictureSelector = ({
     error: "#EF4444",
     progress: "#FACC15",
     placeholder: "#BCBEC0",
+    text: "#fafafa",
+    textDisabled: "#e6e6e6",
   },
   apiBaseUrl = "BASE_URL_SERVICES", // Configurable base URL
   showProgressRing = true, // Show progress ring
+  blurOnProgress = false,
   enableAbortController = true, // Enable/disable abort controller
   testMode = false, // Test mode
   testUploadDelay = 2000, // Upload simulation delay in test mode (milliseconds)
@@ -43,6 +48,7 @@ const PictureSelector = ({
   colors?: ColorPalette;
   apiBaseUrl?: string;
   showProgressRing?: boolean;
+  blurOnProgress: boolean;
   enableAbortController?: boolean;
   testMode?: boolean;
   testUploadDelay?: number;
@@ -298,7 +304,7 @@ const PictureSelector = ({
               src={imageUrl}
               alt={isCircle ? "Profile" : "Image"}
               className={`w-full h-full object-cover ${
-                isCircle ? "rounded-full" : "rounded-2xl"
+                isCircle ? "rounded-full" : "rounded-3xl"
               }`}
               onError={() => setImgError(true)}
               onClick={() => openImage(imageUrl)}
@@ -307,7 +313,7 @@ const PictureSelector = ({
           ) : (
             <div
               className={`w-full h-full flex items-center justify-center bg-gray-100 ${
-                isCircle ? "rounded-full" : "rounded-2xl"
+                isCircle ? "rounded-full" : "rounded-3xl"
               }`}
             >
               {isCircle ? (
@@ -324,40 +330,54 @@ const PictureSelector = ({
                     cy="52.5"
                     r="52.5"
                     stroke={colors.placeholder}
-                    strokeWidth="0.5"
+                    strokeWidth="2"
                   />
                   <path
                     d="M53 106C38.8432 106 25.5338 100.487 15.5234 90.4766C5.51283 80.4662 0 67.1568 0 53C0 38.8432 5.51283 25.5338 15.5234 15.5234C25.5338 5.51283 38.8432 0 53 0C67.1568 0 80.4662 5.51283 90.4766 15.5234C100.487 25.5338 106 38.8432 106 53C106 67.1568 100.487 80.4662 90.4766 90.4766C80.4662 100.487 67.1568 106 53 106ZM53 4.14062C26.0588 4.14062 4.14062 26.0588 4.14062 53C4.14062 79.9412 26.0588 101.859 53 101.859C79.9412 101.859 101.859 79.9412 101.859 53C101.859 26.0588 79.9412 4.14062 53 4.14062Z"
                     fill={colors.placeholder}
                     stroke={colors.placeholder}
-                    strokeWidth="0.5"
+                    strokeWidth="2"
                   />
                   <path
                     d="M53 62.0538C41.6963 62.0538 32.5 52.8577 32.5 41.554C32.5 30.2503 41.6961 21.054 53 21.054C64.3039 21.054 73.5001 30.2501 73.5001 41.554C73.5001 52.8577 64.3037 62.0538 53 62.0538ZM53 25.1946C43.9795 25.1946 36.6406 32.5334 36.6406 41.554C36.6406 50.5745 43.9795 57.9132 53 57.9132C62.0206 57.9132 69.3594 50.5745 69.3594 41.554C69.3594 32.5334 62.0206 25.1946 53 25.1946Z"
                     fill={colors.placeholder}
                     stroke={colors.placeholder}
-                    strokeWidth="0.5"
+                    strokeWidth="2"
                   />
                   <path
                     d="M88.4081 91.6778C87.4235 91.6778 86.5507 90.9731 86.3724 89.9698C83.4945 73.7943 69.4594 62.0537 53 62.0537C36.5406 62.0537 22.5057 73.794 19.6276 89.9698C19.4274 91.0956 18.3527 91.8459 17.2267 91.6455C16.1008 91.4453 15.3505 90.3702 15.5509 89.2446C17.0956 80.5627 21.6741 72.6276 28.443 66.9005C35.293 61.1049 44.014 57.9131 53 57.9131C61.986 57.9131 70.707 61.1049 77.557 66.9005C84.3259 72.6276 88.9044 80.5627 90.4491 89.2446C90.6495 90.3702 89.8992 91.4451 88.7733 91.6455C88.6508 91.6673 88.5284 91.6778 88.4081 91.6778Z"
                     fill={colors.placeholder}
                     stroke={colors.placeholder}
-                    strokeWidth="0.5"
+                    strokeWidth="2"
                   />
                 </svg>
               ) : (
-                // Image placeholder SVG
                 <svg
-                  width={size * 0.4}
-                  height={size * 0.4}
+                  width={size}
+                  height={size}
                   viewBox="0 0 32 32"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path
-                    d="M368,109 C366.896,109 366,108.104 366,107 C366,105.896 366.896,105 368,105 C369.104,105 370,105.896 370,107 C370,108.104 369.104,109 368,109 L368,109 Z M368,103 C365.791,103 364,104.791 364,107 C364,109.209 365.791,111 368,111 C370.209,111 372,109.209 372,107 C372,104.791 370.209,103 368,103 L368,103 Z M390,116.128 L384,110 L374.059,120.111 L370,116 L362,123.337 L362,103 C362,101.896 362.896,101 364,101 L388,101 C389.104,101 390,101.896 390,103 L390,116.128 L390,116.128 Z M390,127 C390,128.104 389.104,129 388,129 L382.832,129 L375.464,121.535 L384,112.999 L390,118.999 L390,127 L390,127 Z M364,129 C362.896,129 362,128.104 362,127 L362,126.061 L369.945,118.945 L380.001,129 L364,129 L364,129 Z M388,99 L364,99 C361.791,99 360,100.791 360,103 L360,127 C360,129.209 361.791,131 364,131 L388,131 C390.209,131 392,129.209 392,127 L392,103 C392,100.791 390.209,99 388,99 L388,99 Z"
-                    fill={colors.placeholder}
-                  />
+                  <defs></defs>
+                  <g
+                    id="Page-1"
+                    stroke="none"
+                    strokeWidth="1"
+                    fill="none"
+                    fillRule="evenodd"
+                  >
+                    <g
+                      id="Icon-Set"
+                      transform="translate(-360.000000, -99.000000)"
+                      fill="#BCBEC0"
+                    >
+                      <path
+                        d="M368,109 C366.896,109 366,108.104 366,107 C366,105.896 366.896,105 368,105 C369.104,105 370,105.896 370,107 C370,108.104 369.104,109 368,109 L368,109 Z M368,103 C365.791,103 364,104.791 364,107 C364,109.209 365.791,111 368,111 C370.209,111 372,109.209 372,107 C372,104.791 370.209,103 368,103 L368,103 Z M390,116.128 L384,110 L374.059,120.111 L370,116 L362,123.337 L362,103 C362,101.896 362.896,101 364,101 L388,101 C389.104,101 390,101.896 390,103 L390,116.128 L390,116.128 Z M390,127 C390,128.104 389.104,129 388,129 L382.832,129 L375.464,121.535 L384,112.999 L390,118.999 L390,127 L390,127 Z M364,129 C362.896,129 362,128.104 362,127 L362,126.061 L369.945,118.945 L380.001,129 L364,129 L364,129 Z M388,99 L364,99 C361.791,99 360,100.791 360,103 L360,127 C360,129.209 361.791,131 364,131 L388,131 C390.209,131 392,129.209 392,127 L392,103 C392,100.791 390.209,99 388,99 L388,99 Z"
+                        id="image-picture"
+                      ></path>
+                    </g>
+                  </g>
                 </svg>
               )}
             </div>
@@ -365,29 +385,58 @@ const PictureSelector = ({
 
           {/* Progress ring - only show for circle (profile) type */}
           {showProgressRing &&
+          uploadProgress > 0 &&
+          uploadProgress < 100 &&
+          isCircle ? (
+            <svg
+              className="absolute z-[6] top-0.5 left-0 pointer-events-none"
+              width={size - 3}
+              height={size - 3}
+              viewBox={`-2 -2 ${size + 4} ${size + 4}`}
+            >
+              <circle
+                cx={radius}
+                cy={radius}
+                r={radius - 6.5 / 2}
+                fill="none"
+                stroke={colors.progress}
+                strokeWidth={10}
+                strokeDasharray={circumference}
+                strokeDashoffset={strokeDashoffset}
+                transform={`rotate(-90 ${radius} ${radius})`}
+                style={{ transition: "stroke-dashoffset 0.3s ease" }}
+              />
+            </svg>
+          ) : (
+            showProgressRing &&
             uploadProgress > 0 &&
-            uploadProgress < 100 &&
-            isCircle && (
-              <svg
-                className="absolute top-0 left-0 pointer-events-none"
-                width={size}
-                height={size}
-                viewBox={`0 0 ${size} ${size}`}
-              >
-                <circle
-                  cx={radius}
-                  cy={radius}
-                  r={radius - 6.5 / 2}
-                  fill="none"
-                  stroke={colors.progress}
-                  strokeWidth={6.5}
-                  strokeDasharray={circumference}
-                  strokeDashoffset={strokeDashoffset}
-                  transform={`rotate(-90 ${radius} ${radius})`}
-                  style={{ transition: "stroke-dashoffset 0.3s ease" }}
-                />
-              </svg>
-            )}
+            uploadProgress < 100 && (
+              <div className="">
+                <svg
+                  className="absolute top-0 z-[6] left-0"
+                  width="180px"
+                  height="180px"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    className="rounded-lg overflow-hidden"
+                    x="1"
+                    y="1"
+                    rx={3}
+                    width="30"
+                    height="30"
+                    fill="none"
+                    stroke="#ffd61f"
+                    strokeWidth={"2"}
+                    strokeDasharray={circumference}
+                    strokeDashoffset={strokeDashoffset}
+                    style={{ transition: "stroke-dashoffset 0.3s ease" }}
+                  />
+                </svg>
+              </div>
+            )
+          )}
 
           {!viewOnly && (
             <>
@@ -399,11 +448,14 @@ const PictureSelector = ({
                   bottom: `${buttonPosition}px`,
                   right: `${buttonPosition}px`,
                 }}
-                className="absolute p-1 cursor-pointer rounded-full shadow-lg flex items-center justify-center"
+                className="absolute p-1 cursor-pointer rounded-full shadow-lg flex items-center justify-center z-10"
                 onClick={triggerFileInput}
                 disabled={loading}
               >
-                <HiOutlinePencilSquare color="#fff" size={buttonSize * 0.7} />
+                <HiOutlinePencilSquare
+                  color={colors.text}
+                  size={buttonSize * 0.7}
+                />
               </button>
               {imageUrl && (
                 <button
@@ -414,11 +466,14 @@ const PictureSelector = ({
                     bottom: `${buttonPosition}px`,
                     left: `${buttonPosition}px`,
                   }}
-                  className="absolute p-1 cursor-pointer rounded-full shadow-lg flex items-center justify-center"
+                  className="absolute p-1 cursor-pointer rounded-full shadow-lg flex items-center justify-center z-10"
                   onClick={handleDeleteImage}
                   disabled={loading}
                 >
-                  <MdDeleteOutline color="#fff" size={buttonSize * 0.7} />
+                  <MdDeleteOutline
+                    color={colors.text}
+                    size={buttonSize * 0.7}
+                  />
                 </button>
               )}
             </>
@@ -432,11 +487,14 @@ const PictureSelector = ({
           className="hidden"
           disabled={loading}
         />
-
         {/* Show percentage when no progress ring or not circle */}
-        {loading && (!showProgressRing || !isCircle) && (
-          <div className="absolute inset-0 bg-white bg-opacity-80 backdrop-blur-sm flex items-center justify-center rounded-2xl">
-            <div className="text-gray-800 text-sm font-semibold">
+        {loading && blurOnProgress && (
+          <div
+            className={`absolute mx-auto inset-0  bg-black/20 bg-opacity-80 backdrop-blur-sm flex items-center z-[5] justify-center ${
+              isCircle ? "rounded-full" : "rounded-3xl"
+            } w-[${size}px] h-[${size}px]`}
+          >
+            <div className="text-sm font-semibold text-white">
               {uploadProgress}%
             </div>
           </div>
