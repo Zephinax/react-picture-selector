75891f81e4292df9e5682bcd88a4d0849e2e1970
Zephinax <zephinax@gmail.com>
2025-10-28 16:03:00 +0330

    docs: add basic documents

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -4,6 +4,7 @@ export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: "React Picture Selector"
-    }
+    },
+    githubUrl: "https://github.com/zephinax/react-picture-selector"
   };
 }
