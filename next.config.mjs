5b13063e93f2f290b3869b215d7b1ef95303861b
Zephinax <zephinax@gmail.com>
2025-10-28 10:29:11 +0330

    chore(release): publish gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,18 @@ import { createMDX } from "fumadocs-mdx/next";
 const withMDX = createMDX();
 
 const isProd = process.env.NODE_ENV === "production";
+const repoName = "react-picture-selector";
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true
   },
-  basePath: isProd ? "/react-picture-selector" : "",
-  assetPrefix: isProd ? "/react-picture-selector/" : ""
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : ""
 };
 
 export default withMDX(config);
